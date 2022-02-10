1c3c044dfff8f76c1c5d7569affb8ba13ecc161a
Leshan Patterson <leshan0627@gmail.com>
2022-02-10 19:57:22 +0000

    docs: Create and add components file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      {/* Header */}
+      <header>
+        <nav className="nav">
+          <div>
+            <h1>Pocket Lawyer</h1>
+          </div>
+          <div>
+            <ul className="nav-list">
+              <li>SOS</li>
+              <li>Requests</li>
+              <li>Find Advce</li>
+            </ul>
+          </div>
+        </nav>
       </header>
+
+      {/* Laywers online section*/}
+      <section id="sidebar">
+        <p>The area is for showing lawyers online</p>
+        {/* user first inital */}
+        <article>{/* laywer image */}</article>
+      </section>
+
+      {/* This is the main section */}
+      <section className="mainbody">
+        <div>
+          <form>
+            <p>This area is for the main body</p>
+            <input></input>
+          </form>
+        </div>
+        <div>
+          <body></body>
+        </div>
+      </section>
     </div>
   );
 }
