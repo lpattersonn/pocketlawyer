115e0586c963e611084a198e6852c4bb3d97c607
Adnan-Raja <adnan.saeed.raja@gmail.com>
2022-02-12 23:50:40 -0500

    feat:implement chatbox

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import "./App.css";
+
+import "./Styles/App.css";
 
 import Header from "./components/Header";
 
 import Lawyer from "./components/Lawyer";
 
 import MainBody from "./components/MainBody";
+
+import Chatbox from "./components/Chatbox";
+
+import "./Styles/Chatbox.css";
 
 function App() {
   return (
@@ -18,6 +23,9 @@ function App() {
 
       {/* This is the main section */}
       <MainBody />
+      </div>
+      <div>
+        <Chatbox />
       </div>
     </div> 
   );
---
+++ src/components/Chatbox.js
@@ -0,0 +1,35 @@
+import react from "react";
+
+
+
+export default function Chatbox(props) {
+  return (
+  <section className="chatbox">
+<div className="coontainer">
+  {/* <img src="/w3images/bandmember.jpg" alt="Avatar" style="width:100%;" /> */}
+  <p>Hello. How are you today?</p>
+  <span className="time-right">11:00</span>
+</div>
+
+<div className="coontainer darker">
+  {/* <img src="/w3images/avatar_g2.jpg" alt="Avatar" className="right" style="width:100%;" /> */}
+  <p>Hey! I'm fine. Thanks for asking!</p>
+  <span className="time-left">11:01</span>
+</div>
+
+<div className="coontainer">
+  {/* <img src="/w3images/bandmember.jpg" alt="Avatar" style="width:100%;" /> */}
+  <p>Sweet! So, what do you wanna do today?</p>
+  <span className="time-right">11:02</span>
+</div>
+
+<div className="coontainer darker">
+  {/* <img src="/w3images/avatar_g2.jpg" alt="Avatar" className="right" style="width:100%;" /> */}
+  <p>Nah, I dunno. Play soccer.. or learn more coding perhaps?</p>
+  <span className="time-left">11:05</span>
+</div>
+  </section>
+   
+  )
+
+}
