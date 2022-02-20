76c26af1f1d2409d8b2075635620336ab089ce7f
Leshan <leshan0627@gmail.com>
2022-02-20 04:43:09 -0500

    feat: Implement chatbox

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -2,12 +2,13 @@
 import React from "react";
 import Nav from "./Nav";
 import { ChatEngine } from "react-chat-engine";
-import "./Styles/chat"
+import "../Styles/chat.css";
 
 export default function Chat(porps) {
   return (
     <div>
       <Nav />
+      <ChatEngine height="100vh" projectID="" userName="" userSecret="" />
     </div>
   );
 }
