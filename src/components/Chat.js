c387f502ce1a284f541f72763daf8fd666e23e09
Leshan <leshan0627@gmail.com>
2022-02-20 04:56:56 -0500

    feat: Implement chatbox credentials

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -8,7 +8,12 @@ export default function Chat(porps) {
   return (
     <div>
       <Nav />
-      <ChatEngine height="100vh" projectID="" userName="" userSecret="" />
+      <ChatEngine
+        height="100vh"
+        projectID="3c0dd3f9-55c8-417c-b8ae-63797659eaa8"
+        userName="Walter"
+        userSecret="1234"
+      />
     </div>
   );
 }
