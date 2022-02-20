adc52404d815bdedc23d2353929532a2b9c13649
Leshan <leshan0627@gmail.com>
2022-02-20 04:36:28 -0500

    style: Add chatbox style

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,6 +1,8 @@
 // Imports
 import React from "react";
 import Nav from "./Nav";
+import { ChatEngine } from "react-chat-engine";
+import "./Styles/chat"
 
 export default function Chat(porps) {
   return (
