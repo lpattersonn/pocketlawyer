6f077c7605fd556e65419cf16c8323fb53fa3b6c
Adnan-Raja <adnan.saeed.raja@gmail.com>
2022-02-22 22:33:13 -0500

    feat:create login component

--- src/components/login.js
+++ src/components/login.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import Nav from "./Nav";
 import axios from "axios";
-import "../Styles/login.css";
+import "../Styles/Login.css";
 
 
-export default function Register(props) {
+export default function Login(props) {
   
 
  
@@ -12,9 +12,42 @@ export default function Register(props) {
 
   return (
     <div>
-      <Nav />
-      <div>
-        <img src="" />
+      {/* <Nav /> */}
+      <div className="logo">
+        
+      </div>
+      <div className="mainPage">
+      
+      <section className="loginMain">
+      <div className="logo">
+        <img src="https://github.com/lpattersonn/pocketlawyer/blob/main/public/pocketlawyer.png?raw=true" />
+        </div>
+        <form className="form">
+          <div className="data">
+          <p>Login</p>
+
+          
+
+          <div>
+            <label>Username:</label>
+            <input />
+          </div>
+
+                   <div>
+            <label>Password:</label>
+            <input
+              
+            />
+          </div>
+
+          <div>
+            <button type="button" class="btn btn-light" >
+              Login
+            </button>
+          </div>
+          </div>
+        </form>
+      </section>
       </div>
     </div>
   );
