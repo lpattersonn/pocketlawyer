09364d90d4b536d819603d554174640c1db7682f
Leshan <leshan0627@gmail.com>
2022-02-23 03:10:49 -0500

    feat: Update to nav for user login

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,94 +4,86 @@ import "../Styles/nav.css";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
-
 export default function Nav() {
-  const [username, setusername] = useState("");
-  const [password, setpassword] = useState("");
-  const [loginStatus, setloginStatus] = useState([]);
+  // const [username, setusername] = useState("");
+  // const [password, setpassword] = useState("");
+  // const [loginStatus, setloginStatus] = useState([]);
 
-  const [userNameShow, setUserNameShow] = useState("")
+  //   const [userNameShow, setUserNameShow] = useState("")
 
-  const login = () => {
-    axios.post('/login', {
-       username: username,
-       password: password,
-    })
-    .then((res) => {
-// console.log(res.data)
-setloginStatus(res.data)
-    })
-  }
+  //   const login = () => {
+  //     axios.post('/login', {
+  //        username: username,
+  //        password: password,
+  //     })
+  //     .then((res) => {
+  // // console.log(res.data)
+  // setloginStatus(res.data)
+  //     })
+  //   }
+  // function getUser() {
+  //   return loginStatus.map((arr, key) => {
+  //     return (
+  //       key={key},
+  // <p>{arr.username}</p>
+  //     )
+  //   })
+  // }
 
+  const [user, setUser] = useState({
+    username: "",
+    image: "",
+  });
 
-function getUser() {
-  return loginStatus.map((arr, key) => {
+  useEffect(() => {
+    axios.get("/api/users").then((res) => {
+      console.log(res.data);
+      for (let firstUser of res.data) {
+        console.log("THIS", firstUser);
+        if (localStorage.getItem("username") === firstUser.username) {
+          setUser({
+            username: firstUser.username,
+            image: firstUser.img,
+          });
+        }
+      }
+    });
+  });
+
+  const logout = function () {
+    localStorage.setItem("username", "");
+    window.location.reload();
+  };
+
+  if (localStorage.getItem("username")) {
     return (
-      key={key},
-<p>{arr.username}</p>
-    )
-  })
-}
-  
-
-
-  return (
-    <nav id="nav">
-      <div id="title">
-      <a href="/">
-        <img
-          className="logo"
-          src="https://github.com/lpattersonn/pocketlawyer/blob/main/public/pocketlawyer.png?raw=true"
-        />
-        </a>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/sos">SOS</Link>
-          </li>
-        </ul>
-      </div>
-      <div className="loginArea">
-        <ul id="login">
-          <div>
-            <form>
-            <li className="login">
-           
-           <div> <input   className="myInput" type="text" placeholder="Username.." 
-           onChange={(e) => {
-            setusername(e.target.value);
-          }}
-           /> </div>
-           <div> <input className="myInput" type="password" placeholder="Password.." 
-           onChange={(e) => {
-            setpassword(e.target.value);
-          }}
-           /></div>
-           <div><button type="button" class="btn btn-light" 
-           onClick={() => {
-             {login()}
-             
-              setUserNameShow(getUser())
-            }}
-            >Login</button>  </div>
-            
+      <nav id="nav">
+        <div id="title">
+          <a href="/">
+            <img
+              className="logo"
+              src="https://github.com/lpattersonn/pocketlawyer/blob/main/public/pocketlawyer.png?raw=true"
+            />
+          </a>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
             </li>
-            </form>
-            </div>
-          <li>
-          {userNameShow}
-          </li>
-            
-          <li>
-          <Link to="/register">Register</Link>
+            <li>
+              <Link to="/sos">SOS</Link>
             </li>
-        </ul>
-        
-       
-        
-      </div>
-    </nav>
-  );
+            <li onClick={logout}>
+              <a href="#">Logout</a>
+            </li>
+          </ul>
+        </div>
+        <div className="loginArea">
+          <p>Hello, {user.username}</p>
+          <div className="loginDiv">
+            <img src={user.image} />
+          </div>
+        </div>
+      </nav>
+    );
+  }
 }
