d5c63aff56b7133be2712ae6b0ed3948a59357c4
Leshan <leshan0627@gmail.com>
2022-02-24 11:45:52 -0500

    chore: Update to lawyer sidebar to chow choose a lawyer message

--- src/components/Lawyer.js
+++ src/components/Lawyer.js
@@ -1,43 +1,42 @@
-import "../Styles/lawyer.css"
+import "../Styles/lawyer.css";
 
 export default function Lawyer(props) {
+  const lawyerCount = props.lawyers.length;
 
- const lawyerCount = props.lawyers.length
-
-// Individual Lawyers
+  // Individual Lawyers
   const lawyerList = props.lawyers.map((lawyer) => {
-
     return (
       <div>
         {/* <div id={lawyer.name}>
       </div> */}
-      <div className="lawyers-list"  onClick={() => {props.setLawyerPopUp({
-        name: lawyer.name,
-        img: lawyer.img,
-        focus: lawyer.focus,
-        description: lawyer.description,
-        style: "temp"
-      })}} >
-        <img src={lawyer.img} alt={lawyer.name} class="lawyerImg"/>
-        <p>
-          {lawyer.focus}
-        </p>
-      
-    </div>
-   
-    </div>
-  )
-  })
+        <div
+          className="lawyers-list"
+          onClick={() => {
+            props.setLawyerPopUp({
+              name: lawyer.name,
+              img: lawyer.img,
+              focus: lawyer.focus,
+              description: lawyer.description,
+              style: "temp",
+            });
+          }}
+        >
+          <img src={lawyer.img} alt={lawyer.name} class="lawyerImg" />
+          <p>{lawyer.focus}</p>
+        </div>
+      </div>
+    );
+  });
 
-// Lawyer list Section 
+  // Lawyer list Section
   return (
     <section id="side-bar">
-      <h4> {lawyerCount} Lawyers Online <br />
-      Choose A Lawyer
+      <h4>
+        <b>{lawyerCount}</b> Lawyers Online <br />
+        Choose A Lawyer
       </h4>
-      
-     {lawyerList}
-     
-      </section>
-  )
+
+      {lawyerList}
+    </section>
+  );
 }
